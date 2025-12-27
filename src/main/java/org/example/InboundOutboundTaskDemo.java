1fb395affa542fb2115c6d366a7789aeebe40ecc
manojmadushanka <madushankamanoj414@gmail.com>
2025-12-27 19:40:01 +0530

    virtual thread demo

--- src/main/java/org/example/InboundOutboundTaskDemo.java
+++ src/main/java/org/example/InboundOutboundTaskDemo.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.concurrent.CountDownLatch;
+
 public class InboundOutboundTaskDemo {
     private static final int MAX_THREADS = 500;
+    private static final int MAX_V_THREADS = 5000;
 
     static void main(String[] args) {
         platformThreadDaemonDemo();
@@ -11,6 +14,17 @@ public class InboundOutboundTaskDemo {
         for (int i = 0; i < MAX_THREADS; i++) {
             int j = i;
             Thread thread = new Thread(() -> {
+                PlatformThreadLimit.ioIntensive(j);
+            });
+            thread.start();
+        }
+    }
+
+    private static void platformThreadUsingBuilderDemo() {
+        var builder = Thread.ofPlatform().name("platformThreadDemo", 1);
+        for (int i = 0; i < MAX_THREADS; i++) {
+            int j = i;
+            Thread thread = builder.unstarted(() -> {
                 PlatformThreadLimit.ioIntensive(j);
             });
             thread.start();
@@ -30,6 +44,24 @@ public class InboundOutboundTaskDemo {
                 thread.start();
             }
             latch.await(); // we can also use join on each thread
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void virtualThreadDemo(){
+        var latch = new CountDownLatch(MAX_V_THREADS);
+        var builder = Thread.ofVirtual();
+        for(int i = 0;i<MAX_V_THREADS;i++){
+            int j = i;
+            Thread thread = builder.unstarted(()->{
+                PlatformThreadLimit.ioIntensive(j);
+                latch.countDown();
+            });
+            thread.start();
+        }
+        try {
+            latch.await();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
